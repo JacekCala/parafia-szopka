db95444779600ba8ad3745b982ca153f66e51d3a
Jacek Cala <Jacek.Cala@gmail.com>
2023-12-11 22:17:30 +0000

    Small fixes to the startup script

--- uruchom-szopke.bat
+++ uruchom-szopke.bat
@@ -1,19 +1,17 @@
-%echo on
+@echo off
 
 cd d:\projects\parafia-szopka
 
-echo Directory: %CD%
-echo Path: %PATH%
+rem # echo Directory: %CD%
+rem # echo Path: %PATH%
 
 where /q poetry
 
 if ERRORLEVEL 1 (
-    echo Using venv
+    rem # echo Using venv
     .venv/Scripts/activate.bat
     python szopka_na_ruch.py
 ) else (
-    echo Using poetry
+    rem # echo Using poetry
     poetry run szopka_na_ruch.py
 )
-
-echo Done.
